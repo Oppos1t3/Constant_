e51441f1eeb4543529bfc53ae5a61cdc81305f58
Oppos1t3 <132361526+Oppos1t3@users.noreply.github.com>
2023-05-02 13:47:03 +0100

    Oppos1t3....READ.ME

---
+++ README.m
@@ -0,0 +1,12 @@
+- 👋 Hi, I’m @Oppos1t3
+- 👀 I’m interested in ...Coding,Anime,Sports,Sad music and Gaming.
+- 🌱 I’m currently learning ...Python,CSS,C++,HTML
+- 💞️ I’m looking to collaborate on ...Coding projects buh I am just a begginer
+- 📫 How to reach me ...ayobamioladiyun@gmail.com(discord ltrrr)
+I wanna be the next Elon Musk
+13yrs.......
+lez go
+<!---
+Oppos1t3/Oppos1t3 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
